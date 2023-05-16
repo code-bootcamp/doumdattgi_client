db644f716589fa01b9b2ea42f95ea06ceb0097db
cometes <vloslain@gmail.com>
2023-05-16 11:07:05 +0900

    Feature: 로그인 기능, 마이페이지 설정 기능 구현

--- src/commons/styles/globalStyle.ts
+++ src/commons/styles/globalStyle.ts
@@ -2,37 +2,6 @@ import { css } from "@emotion/react";
 
 export const globalStyle = css`
 
-//   /* 결제 모달창 CSS */
-//   .ant-space {
-//     width: 384px;
-//     height: 34px;
-//     border: none;
-//     border-bottom: 2px solid black;
-//     padding: 0px 5px;
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: space-between;
-//     color: black;
-//   }
-
-//   /* 결제 드랍박스 CSS */
-//   .ant-space-item {
-//     color: #828282;
-//   }
-
-//   svg {
-//     color: black;
-//   }
-
-//   .ant-dropdown-menu-item {
-//     height: 50px;
-//   }
-
-//   a::after {
-//     border-bottom: 1px solid rgba(5, 5, 5, 0.1);
-//   }
-// `;
-
     *{
         margin: 0;
         padding: 0;
