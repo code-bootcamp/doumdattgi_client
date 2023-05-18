3d5a08efcaad4e43aaac0b90cbbfb55c70018c44
wooryeol <dnfuf0703@gmail.com>
2023-05-18 13:54:51 +0900

    feature-comment

--- src/components/units/comment/index.tsx
+++ src/components/units/comment/index.tsx
@@ -1,9 +1,33 @@
+import InputHeight50px from "../../commons/inputs/InputHeight50px";
 import * as S from "./styles";
 
 export default function Comment(): JSX.Element {
   return (
     <S.Wrapper>
-      <></>
+      <S.WrapperHeader>
+        <S.Close src="/close.png" />
+      </S.WrapperHeader>
+
+      <S.WrapperBody>
+        <S.ReceivingBox>
+          <S.Date>23.05.10</S.Date>
+          <S.ChatBox>팬이에요</S.ChatBox>
+        </S.ReceivingBox>
+
+        <S.SendingBox>
+          <S.SenderIcon src="/IU.jpeg" />
+          <div>
+            <S.Sender>아이유</S.Sender>
+            <S.ChatBox>안녕</S.ChatBox>
+          </div>
+          <S.Date>23.05.10</S.Date>
+        </S.SendingBox>
+      </S.WrapperBody>
+
+      <S.WrapperFooter>
+        <InputHeight50px placeholder="메세지를 입력하세요" />
+        <S.SendingBtn>전송</S.SendingBtn>
+      </S.WrapperFooter>
     </S.Wrapper>
   );
 }
