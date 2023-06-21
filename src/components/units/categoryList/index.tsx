dfde782b657c8d796e19c4a5d202b6a518322668
songhyunseop <gustjq99455352@gmail.com>
2023-06-21 11:52:28 +0900

    Update: 페이지 수정 및 리팩토링2

--- src/components/units/categoryList/index.tsx
+++ src/components/units/categoryList/index.tsx
@@ -1,7 +1,6 @@
 import SideCategory from "../../commons/parts/list/category";
 import ProductList from "../../commons/parts/list/list";
 import * as S from "./styles";
-//
 
 export default function ListUnit() {
   return (
