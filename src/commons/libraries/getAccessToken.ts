3ace9fb4095d5ccf47e9f8606fb64ccac01edc40
songhyunseop <gustjq99455352@gmail.com>
2023-07-12 21:08:54 +0900

    Update: 코드수정

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -13,12 +13,17 @@ export const getAccessToken = async (): Promise<string | undefined> => {
       "https://doumdattgi-server.com/graphql",
       { credentials: "include" }
     );
-    const result: any = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
 
+    const result: any = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
     const newAccessToken = result.restoreAccessToken;
+
+    console.log("이건 토큰이요");
+    console.log(newAccessToken);
+    console.log("이건 토큰이요");
 
     return newAccessToken;
   } catch (error: any) {
+    console.log("이거 에러임");
     console.log(error.message);
     console.log("에러");
   }
