bb20f49688e926ad33f8e8e3841290b3a40ec8d8
cometes <vloslain@gmail.com>
2023-11-06 17:00:27 +0900

    test-mobile-width

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,21 +1,5 @@
-import { useEffect, useState } from "react";
 import Login from "../../src/components/units/login";
 
 export default function LoginPage(): JSX.Element {
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const resizeListener = () => {
-      setInnerWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", resizeListener);
-  });
-
-  console.log("innerWidth", innerWidth);
-  return (
-    <>
-      <div>{`현재 넓이는:${innerWidth}px 입니다`}</div>
-      <Login />
-    </>
-  );
+  return <Login />;
 }
